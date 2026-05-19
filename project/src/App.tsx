968a1621ab5bbad6e2668e882a6906493c09a32c
platstarinum-prog <platstarinum@gmail.com>
2026-05-19 21:57:41 +0300

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,9 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// Добавь этот импорт:
-import { LanguageProvider } from './context/LanguageContext'; 
-
 import Navbar from './components/Navbar';
-import LanguageSwitcher from './components/LanguageSwitcher';
 import HomePage from './pages/HomePage';
 import CatalogPage from './pages/CatalogPage';
 import ReviewsPage from './pages/ReviewsPage';
@@ -11,19 +7,16 @@ import FaqPage from './pages/FaqPage';
 
 export default function App() {
   return (
-    <LanguageProvider>
-      <BrowserRouter>
-        <div className="min-h-screen bg-black text-white font-sans antialiased">
-          <Navbar />
-          <LanguageSwitcher />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/reviews" element={<ReviewsPage />} />
-            <Route path="/faq" element={<FaqPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </LanguageProvider>
+    <BrowserRouter>
+      <div className="min-h-screen bg-black text-white font-sans antialiased">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/reviews" element={<ReviewsPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
