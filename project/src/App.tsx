592f12abb37d3523e4be563aa1f9df5616cc061b
platstarinum-prog <platstarinum@gmail.com>
2026-05-20 17:01:22 +0300

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import CatalogPage from './pages/CatalogPage';
 import ReviewsPage from './pages/ReviewsPage';
@@ -8,14 +9,17 @@ import FaqPage from './pages/FaqPage';
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-black text-white font-sans antialiased">
+      <div className="min-h-screen bg-black text-white font-sans antialiased flex flex-col">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/reviews" element={<ReviewsPage />} />
-          <Route path="/faq" element={<FaqPage />} />
-        </Routes>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/reviews" element={<ReviewsPage />} />
+            <Route path="/faq" element={<FaqPage />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
